a7b71a6009f9f2adea394f142d827eacaa39f904
Tamarat <ililiioiwio@gmail.com>
2026-02-03 21:04:37 +0700

    add Exit

--- main/calculator.cpp
+++ main/calculator.cpp
@@ -4,28 +4,41 @@ int main() {
     int num;
     int result;
     char op;
+    char exit = 1;
 
     printf("Calculator : ");
     scanf("%d", &result);
 
     while(1) {
-        int c = getchar();
+        while(1) {
+            int c = getchar();
 
-        if(c == '\n') {
-            break;
+            if(c == '\n') {
+                break;
+            }
+
+            op = c;
+
+            scanf("%d", &num);
+
+            if (op == '+') result += num;
+            else if (op == '-') result -= num;
+            else if (op == '*') result *= num;
+            else if (op == '/') result /= num;
+            else if (op == '%') result %= num;
+
+            printf("Exit 0, don't Exit 1 :");
+            scanf("%c", &exit);
+
         }
+        
+        printf("\n");
+        printf("result = %d", result);
 
-        op = c;
-
-        scanf("%d", &num);
-
-        if (op == '+') result += num;
-        else if (op == '-') result -= num;
-        else if (op == '*') result *= num;
-        else if (op == '/') result /= num;
-        else if (op == '%') result %= num;
+        if(exit == 0) {
+                break;
+            }
     }
 
-    printf("\n");
-    printf("result = %d", result);
+    
 }
