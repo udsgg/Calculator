6cef957135fd3cccd4f22dea3f465684f7df3c09
Tamarat <ililiioiwio@gmail.com>
2026-02-03 18:44:56 +0700

    update calculator

---
+++ main/calculator.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    int result;
+    char op;
+
+    printf("Calculator : ");
+    scanf("%d", &result);
+
+    while(1) {
+        int c = getchar();
+
+        if(c == '\n') {
+            break;
+        }
+
+        op = c;
+
+        scanf("%d", &num);
+
+        if (op == '+') result += num;
+        else if (op == '-') result -= num;
+        else if (op == '*') result *= num;
+        else if (op == '/') result /= num;
+        else if (op == '%') result %= num;
+    }
+
+    printf("\n");
+    printf("result = %d", result);
+}
